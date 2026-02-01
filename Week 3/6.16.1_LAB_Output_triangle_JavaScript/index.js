234c428833652bb466da223be2b85473dc17dc36
Jasmine <jsande4861@my.sullivan.edu>
2026-01-31 19:53:29 -0500

    make JS display as webpage

--- Week 3/6.16.1_LAB_Output_triangle_JavaScript/index.js
+++ Week 3/6.16.1_LAB_Output_triangle_JavaScript/index.js
@@ -1,12 +1,18 @@
+// Reference the HTML
+const displayArea = document.getElementById('display-area');
+
+
 function drawTriangle(triangleSize) {
 
    // TODO: Add your solution here
    for (let i = 1; i <= triangleSize; i++) {
       console.log('*'.repeat(i));
    }
-}
 
-console.log("Testing drawTriangle()...");
+   // Write to the webpage instead of (or in addition to) the console
+   console.log(message);
+   displayArea.innerHTML += message + "<br>"; 
+}
 
 // TODO: Test drawTriangle() with different arguments
 drawTriangle(4);
