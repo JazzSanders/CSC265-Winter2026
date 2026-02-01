582ee441925fbd0c7c8d291ae84665d4c1538341
Jasmine <jsande4861@my.sullivan.edu>
2026-01-31 20:00:59 -0500

    display JS update

--- Week 3/6.15.1_LAB_Print_sum_JavaScript/index.js
+++ Week 3/6.15.1_LAB_Print_sum_JavaScript/index.js
@@ -1,39 +1,36 @@
-// Reference the HTML
+// Reference the HTML element
 const displayArea = document.getElementById('display-area');
 
-
 function printSum(x, y) {
-   // TODO: Add your solution here
    const numX = parseFloat(x);
    const numY = parseFloat(y);
 
    const xIsNaN = isNaN(numX);
    const yIsNaN = isNaN(numY);
 
+   let message = ""; // Variable to hold the string
+
    if (xIsNaN && yIsNaN) {
-      console.log(`'${x}' and '${y}' are not numbers.`);
+      message = `'${x}' and '${y}' are not numbers.`;
    } else if (xIsNaN) {
-      console.log(`'${x}' is not a number.`);
+      message = `'${x}' is not a number.`;
    } else if (yIsNaN) {
-      console.log(`'${y}' is not a number.`);
+      message = `'${y}' is not a number.`;
    } else {
       const sum = numX + numY;
-      console.log(`Sum is ${sum}.`);
+      message = `Sum is ${sum}.`;
    }
 
-
-// Write to the webpage instead of (or in addition to) the console
+   // Write to the webpage instead of (or in addition to) the console
    console.log(message);
    displayArea.innerHTML += message + "<br>"; 
 }
 
 // Running the tests
-printSum(3, 6);            // 9
-printSum(3.5, 6.1);        // 9.6
-printSum("hello", 6);      // 'hello' is not a number
-printSum(10, "hi");        // 'hi' is not a number
-printSum("hello", "hi");   // 'hello' and 'hi' are not numbers
+printSum(3, 6);
+printSum(3.5, 6.1);
+printSum("hello", 6);
+printSum(10, "hi");
+printSum("hello", "hi");
 
-
-// Do NOT remove the following line
 export default printSum;
